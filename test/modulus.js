322f8fb92584f4fcfdd908a0ecdb0c65e66e244c
Rahul Ghangas <rahul@atomlabs.one>
2022-01-09 20:12:00 +0000

    test: refactor modulus test

--- test/modulus.js
+++ test/modulus.js
@@ -1,6 +1,9 @@
 const chai = require("chai");
 const path = require("path");
 const assert = require("assert");
+const bigintModArith = require('bigint-mod-arith');
+const utils = require('./utils');
+const { performance } = require('perf_hooks');
 
 const wasm_tester = require("circom_tester").wasm;
 // const snarkjs = require("snarkjs");
@@ -11,29 +14,40 @@ describe("Modulus Test", () => {
 
 		it("should add them correctly when sum is over global prime in circom", async () => {
 			const cir = await wasm_tester(path.join(__dirname, "circuits", "modulus1.circom"));
-			const a = BigInt("157896044618658097711785492504343953926634992332820282019728792003956564819949");
-			const buf = Buffer.from(a.toString(16), 'hex').reverse();
-			const asBits = buffer2bits(buf);
+			const a = BigInt("44618658097711785492504343953926634992332820282019728792003956564819949");
+			const buf = utils.bigIntToLEBuffer(a);
+			const asBits = utils.buffer2bits(buf);
 			const witness = await cir.calculateWitness({ "a": asBits}, true);
 
-			const expected = buffer2bits(Buffer.from((a%p).toString(16), 'hex').reverse());
+			const expected = utils.pad(utils.buffer2bits(utils.bigIntToLEBuffer(bigintModArith.modPow(a, 1, p))), 255);
+			assert.ok(witness.slice(1, 256).every((u, i) => {
+				return u === expected[i];
+			}));
+		});
+	});
+
+	describe("when performing modular additon on two numbers using prime field of prime 25519", () => {
+		const p = BigInt("57896044618658097711785492504343953926634992332820282019728792003956564819949");
+
+		it("should add them correctly when sum is over global prime in circom", async () => {
+			const cir = await wasm_tester(path.join(__dirname, "circuits", "modulus2.circom"));
+			const a = BigInt("1257896044618658097711785492504343953926634992332820282019728792003956564819949");
+			const buf = utils.bigIntToLEBuffer(a);
+			const asBits = utils.buffer2bits(buf);
+			console.log(asBits.length);
+			var startTime = performance.now();
+			const witness = await cir.calculateWitness({ "a": asBits}, true);
+			var endTime = performance.now();
+			console.log(`Call to doSomething took ${endTime - startTime} milliseconds`);
+
+
+			const expected = utils.pad(utils.buffer2bits(utils.bigIntToLEBuffer(bigintModArith.modPow(a, 1, p))), 264);
+			for (var i=0; i<255; i++) {
+				console.log(witness[i+1], expected[i]);
+			}
 			assert.ok(witness.slice(1, 255).every((u, i) => {
 				return u === expected[i];
 			}));
 		});
 	});
 });
-
-function buffer2bits(buff) {
-	const res = [];
-	for (let i=0; i<buff.length; i++) {
-			for (let j=0; j<8; j++) {
-					if ((buff[i]>>j)&1) {
-							res.push(1n);
-					} else {
-							res.push(0n);
-					}
-			}
-	}
-	return res;
-}
