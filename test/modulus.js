a0402e5a3702d0858f8c60a29f8a242bb3024a42
Rahul Ghangas <rahul@atomlabs.one>
2022-01-10 11:27:18 +0000

    test: test for tempalte ModulusAgainst2P

--- test/modulus.js
+++ test/modulus.js
@@ -9,6 +9,41 @@ const wasm_tester = require("circom_tester").wasm;
 // const snarkjs = require("snarkjs");
 
 describe("Modulus Test", () => {
+
+	describe("when performing modular additon on two numbers using prime field of prime 25519", () => {
+		const p = BigInt("57896044618658097711785492504343953926634992332820282019728792003956564819949");
+
+		it("should add them correctly when sum is over global prime in circom", async () => {
+			const cir = await wasm_tester(path.join(__dirname, "circuits", "modulus0.circom"));
+			const a = BigInt("107896044618658097711785492504343953926634992332820282019728792003956564819949");
+			const buf = utils.bigIntToLEBuffer(a);
+			const asBits = utils.buffer2bits(buf);
+			const witness = await cir.calculateWitness({ "in": asBits}, true);
+
+			const expected = utils.pad(utils.buffer2bits(utils.bigIntToLEBuffer(bigintModArith.modPow(a, 1, p))), 255);
+			assert.ok(witness.slice(1, 256).every((u, i) => {
+				return u === expected[i];
+			}));
+		});
+	});
+
+	describe("when performing modular additon on two numbers using prime field of prime 25519", () => {
+		const p = BigInt("57896044618658097711785492504343953926634992332820282019728792003956564819949");
+
+		it("should add them correctly when sum is over global prime in circom", async () => {
+			const cir = await wasm_tester(path.join(__dirname, "circuits", "modulus0.circom"));
+			const a = BigInt("3953926634992332820282019728792003956564819949");
+			const buf = utils.bigIntToLEBuffer(a);
+			const asBits = utils.pad(utils.buffer2bits(buf), 256);
+			const witness = await cir.calculateWitness({ "in": asBits}, true);
+
+			const expected = utils.pad(utils.buffer2bits(utils.bigIntToLEBuffer(bigintModArith.modPow(a, 1, p))), 255);
+			assert.ok(witness.slice(1, 256).every((u, i) => {
+				return u === expected[i];
+			}));
+		});
+	});
+
 	describe("when performing modular additon on two numbers using prime field of prime 25519", () => {
 		const p = BigInt("57896044618658097711785492504343953926634992332820282019728792003956564819949");
 
