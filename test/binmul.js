59467a20c8cdaa05f3ba4534ae72801d82f6154d
Rahul Ghangas <rahul@atomlabs.one>
2022-01-14 13:10:40 +0000

    test: add test for fast binary multiplication

--- test/binmul.js
+++ test/binmul.js
@@ -24,3 +24,27 @@ describe("Binary Multiplier Test", () => {
 		});
 	});
 });
+
+describe("Fast Binary Multiplier Test", () => {
+	describe("when performing binary multiplication on 104 bit and an 40 bit numbers", () => {
+		it("should multiply them correctly", async () => {
+			const cir = await wasm_tester(path.join(__dirname, "circuits", "binmulfast1.circom"));
+			const a = BigInt("2820282019728792003956564819949");
+			const b = BigInt("956564819949");
+			const buf1 = utils.bigIntToLEBuffer(a);
+			const asBits1 = utils.buffer2bits(buf1);
+			const buf2 = utils.bigIntToLEBuffer(b);
+			const asBits2 = utils.buffer2bits(buf2);
+			const witness = await cir.calculateWitness({ "in1": asBits1, "in2": asBits2}, true);
+
+			const expected = utils.normalize(utils.buffer2bits(utils.bigIntToLEBuffer(a*b)));
+			// console.log(expected.length);
+			for (var i=0; i<144; i++) {
+				console.log(witness[i+1], expected[i]);
+			}
+			assert.ok(witness.slice(1, 145).every((u, i) => {
+				return u === expected[i];
+			}));
+		});
+	});
+});
