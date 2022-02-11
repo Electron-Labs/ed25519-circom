136038e1205d727c92b694fa56d5207c29dd9db1
Rahul Ghangas <rahul@atomlabs.one>
2022-02-11 11:24:17 +0000

    test: change test name and expected output

--- test/binmul.js
+++ test/binmul.js
@@ -82,8 +82,8 @@ describe("Check bits less then 51",()=>{
 		});
 	});
 
-	describe("when a number is passed into it of 52 bits",() =>{
-		it("should give output of 0", async ()=>{
+	describe("when a number is passed into it of greater than 52 bits",() =>{
+		it("should fail on witness calculation", async ()=>{
 			const cir = await wasm_tester(path.join(__dirname,"circuits","binmullessthan51.circom"));
 			try {
 				await cir.calculateWitness({"in": BigInt('45035996273704904503599627370490')});
