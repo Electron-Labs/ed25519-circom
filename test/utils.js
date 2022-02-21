04a3e627c0ffcf4f62aee51fffbf2411c0d103e2
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2022-02-21 20:47:49 +0530

    chore: added the point-multiplication utility in utils output checked with the python implementation

--- test/utils.js
+++ test/utils.js
@@ -42,13 +42,16 @@ function pad(x, n) {
   }
   return x;
 }
-
+// This function will give the right modulud as expected 
+function modulus(num, p){
+  return ((num%p)+p)%p;
+}
 // This function will convert a bigInt into the chucks of Integers
 function chunkBigInt(n, mod=BigInt(2**51)){
 	if (!n) return [0];
 	let arr = [];
 	while (n) {
-		arr.push(BigInt(n % mod));
+		arr.push(BigInt(modulus(n,mod)));
 		n /= mod;
 	}
 	return arr;
@@ -58,17 +61,28 @@ let p = BigInt(2**255) - BigInt(19);
 let d = 37095705934669439343138083508754565189542113879843219016388785533085940283555n;
 
 function point_add(P,Q){
-    let A =  (P[1]-P[0])*(Q[1]-Q[0])%p;
-    let B =  (P[1]+P[0])*(Q[1]+Q[0])%p; 
-    let C =  BigInt(2)*P[3]*Q[3]*d % p;
-    let D =  BigInt(2)*P[2]*Q[2] % p;
+  let A =  modulus((P[1]-P[0])*(Q[1]-Q[0]),p);
+  let B =  modulus((P[1]+P[0])*(Q[1]+Q[0]),p); 
+  let C =  modulus(BigInt(2)*P[3]*Q[3]*d , p);
+  let D =  modulus(BigInt(2)*P[2]*Q[2] ,p);
 
-    let E = B-A;
-    let F = D-C;
-    let G = D+C;
-    let H = B+A;
-    
-    return [E*F, G*H, F*G, E*H];
+  let E = B-A;
+  let F = D-C;
+  let G = D+C;
+  let H = B+A;
+  
+  return [E*F, G*H, F*G, E*H];
 }
+function point_mul(s,P){
+	let Q = [0n,1n,1n,0n];
+	while (s > 0){
+		if (s & 1n){
+			Q = point_add(Q,P);
+		}
+		P = point_add(P,P);
+		s >>= 1n;
+	}
+	return Q;
 
-module.exports = { buffer2bits, convertToEvenLength, normalize, bigIntToLEBuffer, pad, chunkBigInt,point_add};
+}
+module.exports = { buffer2bits, convertToEvenLength, normalize, bigIntToLEBuffer, pad, chunkBigInt,point_add,modulus,point_mul};
