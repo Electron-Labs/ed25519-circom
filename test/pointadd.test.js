9f72e405f0621e1490b47f9455a1575e081dd2a9
Rahul Ghangas <rahul@atomlabs.one>
2022-02-23 20:15:55 +0000

    chore: formatting

--- test/pointadd.test.js
+++ test/pointadd.test.js
@@ -4,53 +4,53 @@ const wasmTester = require('circom_tester').wasm;
 const utils = require('./utils');
 
 describe('Point Addition test on ed25519', () => {
-    describe('when performing point addition on the EC of 255 bits ', () => {
-        it('should add them correctly ', async () => {
-            const cir = await wasmTester(path.join(__dirname, 'circuits', 'point-addition51.circom'));
-            const p = BigInt(2 ** 255) - BigInt(19);
-            const P = [
-                15112221349535400772501151409588531511454012693041857206046113283949847762202n,
-                46316835694926478169428394003475163141307993866256225615783033603165251855960n,
-                1n,
-                46827403850823179245072216630277197565144205554125654976674165829533817101731n,
-            ];
-            const Q = [
-                15112221349535400772501151409588531511454012693041857206046113283949847762202n,
-                46316835694926478169428394003475163141307993866256225615783033603165251855960n,
-                1n,
-                46827403850823179245072216630277197565144205554125654976674165829533817101731n,
-            ];
+  describe('when performing point addition on the EC of 255 bits ', () => {
+    it('should add them correctly ', async () => {
+      const cir = await wasmTester(path.join(__dirname, 'circuits', 'point-addition51.circom'));
+      const p = BigInt(2 ** 255) - BigInt(19);
+      const P = [
+        15112221349535400772501151409588531511454012693041857206046113283949847762202n,
+        46316835694926478169428394003475163141307993866256225615783033603165251855960n,
+        1n,
+        46827403850823179245072216630277197565144205554125654976674165829533817101731n,
+      ];
+      const Q = [
+        15112221349535400772501151409588531511454012693041857206046113283949847762202n,
+        46316835694926478169428394003475163141307993866256225615783033603165251855960n,
+        1n,
+        46827403850823179245072216630277197565144205554125654976674165829533817101731n,
+      ];
 
-            const chunk1 = [];
-            const chunk2 = [];
-            for(let i=0;i<4;i++){
-                chunk1.push(utils.chunkBigInt(P[i]));
-                chunk2.push(utils.chunkBigInt(Q[i]));
-            }
-            for(let i=0;i<4;i++){
-                utils.pad(chunk1[i],5);
-                utils.pad(chunk2[i],5);
-            }            
-            const witness = await cir.calculateWitness({"P":chunk1,"Q":chunk2},true);
-            const res  = utils.point_add(P,Q);
-            const expected = [];
-            for(let i=0;i<4;i++){  
-                expected.push((utils.modulus(res[i],p))); 
-            }
-            const wt = witness.slice(1, 21);
-            const chunk= [];
-            for(let i=0;i<4;i++){
-                chunk.push(wt.slice(5*i, 5*i+5));
-            }
-            
-            const dechunked_wt = [];
-            for( let i=0;i<4;i++){
-                dechunked_wt.push(utils.dechunk(chunk[i]));
-            }
-            
-            assert.ok(
-                utils.point_equal(expected,dechunked_wt)
-            );
-        });
+      const chunk1 = [];
+      const chunk2 = [];
+      for (let i = 0; i < 4; i++) {
+        chunk1.push(utils.chunkBigInt(P[i]));
+        chunk2.push(utils.chunkBigInt(Q[i]));
+      }
+      for (let i = 0; i < 4; i++) {
+        utils.pad(chunk1[i], 5);
+        utils.pad(chunk2[i], 5);
+      }
+      const witness = await cir.calculateWitness({ P: chunk1, Q: chunk2 }, true);
+      const res = utils.point_add(P, Q);
+      const expected = [];
+      for (let i = 0; i < 4; i++) {
+        expected.push((utils.modulus(res[i], p)));
+      }
+      const wt = witness.slice(1, 21);
+      const chunk = [];
+      for (let i = 0; i < 4; i++) {
+        chunk.push(wt.slice(5 * i, 5 * i + 5));
+      }
+
+      const dechunkedWt = [];
+      for (let i = 0; i < 4; i++) {
+        dechunkedWt.push(utils.dechunk(chunk[i]));
+      }
+
+      assert.ok(
+        utils.point_equal(expected, dechunkedWt),
+      );
     });
+  });
 });
--- test/scalarmul.test.js
+++ test/scalarmul.test.js
@@ -1,46 +1,48 @@
-const chai = require('chai')
-const path = require('path')
-const assert = require('assert')
-const utils = require('./utils')
-const { LOADIPHLPAPI } = require('dns')
-
-const wasm_tester = require('circom_tester').wasm
+const path = require('path');
+const assert = require('assert');
+const wasmTester = require('circom_tester').wasm;
+const utils = require('./utils');
 
 describe('Scalar multiplication for ed25519', () => {
   describe('when performing scalar multiplication on curve ', () => {
     it('should multiply them correctly', async () => {
-      const cir = await wasm_tester(path.join(__dirname, 'circuits', 'scalarmul.circom'))
-      const p = BigInt(2 ** 255) - BigInt(19)
+      const cir = await wasmTester(path.join(__dirname, 'circuits', 'scalarmul.circom'));
+      const p = BigInt(2 ** 255) - BigInt(19);
       const s = 4869643893319708471955165214975585939793846505679808910535986866633137979160n;
-      const P = [15112221349535400772501151409588531511454012693041857206046113283949847762202n, 46316835694926478169428394003475163141307993866256225615783033603165251855960n, 1n, 46827403850823179245072216630277197565144205554125654976674165829533817101731n]
-      const buf = utils.bigIntToLEBuffer(s)
-      const asBits = utils.pad(utils.buffer2bits(buf), 255)
+      const P = [
+        15112221349535400772501151409588531511454012693041857206046113283949847762202n,
+        46316835694926478169428394003475163141307993866256225615783033603165251855960n,
+        1n,
+        46827403850823179245072216630277197565144205554125654976674165829533817101731n,
+      ];
+      const buf = utils.bigIntToLEBuffer(s);
+      const asBits = utils.pad(utils.buffer2bits(buf), 255);
       asBits.pop();
-      const chunkP = []
+      const chunkP = [];
       for (let i = 0; i < 4; i++) {
-        chunkP.push(utils.chunkBigInt(P[i]))
+        chunkP.push(utils.chunkBigInt(P[i]));
       }
       for (let i = 0; i < 4; i++) {
-        utils.pad(chunkP[i], 5)
+        utils.pad(chunkP[i], 5);
       }
-      const witness = await cir.calculateWitness({ s: asBits, P: chunkP })
-      const res = utils.point_mul(s, P)
+      const witness = await cir.calculateWitness({ s: asBits, P: chunkP });
+      const res = utils.point_mul(s, P);
       for (let i = 0; i < 4; i++) {
-        res[i] = utils.modulus(res[i], p)
+        res[i] = utils.modulus(res[i], p);
       }
-      const wt = witness.slice(1, 21)
-      const chunk = []
+      const wt = witness.slice(1, 21);
+      const chunk = [];
       for (let i = 0; i < 4; i++) {
-        chunk.push(wt.slice(5 * i, 5 * i + 5))
+        chunk.push(wt.slice(5 * i, 5 * i + 5));
       }
 
-      const dechunked_wt = []
+      const dechunkedWt = [];
       for (let i = 0; i < 4; i++) {
-        dechunked_wt.push(utils.dechunk(chunk[i]))
+        dechunkedWt.push(utils.dechunk(chunk[i]));
       }
-     assert.ok(
-         utils.point_equal(res,dechunked_wt)
-     );
-    })
-  })
-})
+      assert.ok(
+        utils.point_equal(res, dechunkedWt),
+      );
+    });
+  });
+});
