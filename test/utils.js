03fe03cf273781328e6fee34c6493794574fba06
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2022-02-23 15:36:22 +0530

    chore : modified utility file used proper js linting and renamed few files

--- test/utils.js
+++ test/utils.js
@@ -1,13 +1,13 @@
 function buffer2bits(buff) {
 	const res = [];
-	for (let i=0; i<buff.length; i++) {
-    for (let j=0; j<8; j++) {
-      if ((buff[i]>>j)&1) {
-        res.push(1n);
-      } else {
-        res.push(0n);
-      }
-    }
+	for (let i = 0; i < buff.length; i++) {
+		for (let j = 0; j < 8; j++) {
+			if ((buff[i] >> j) & 1) {
+				res.push(1n);
+			} else {
+				res.push(0n);
+			}
+		}
 	}
 	return res;
 }
@@ -26,8 +26,7 @@ function normalize(input) {
 	return input;
 }
 
-function IsPowerOfTwo(x)
-{
+function IsPowerOfTwo(x) {
 	return (x & (x - 1)) == 0;
 }
 
@@ -36,70 +35,83 @@ function bigIntToLEBuffer(x) {
 }
 
 function pad(x, n) {
-  var total = n-x.length;
-  for (var i=0; i<total; i++) {
-    x.push(0n);
-  }
-  return x;
+	var total = n - x.length;
+	for (var i = 0; i < total; i++) {
+		x.push(0n);
+	}
+	return x;
 }
 // This function will give the right modulud as expected 
-function modulus(num, p){
-  return ((num%p)+p)%p;
+function modulus(num, p) {
+	return ((num % p) + p) % p;
 }
 
 function bitsToBigInt(arr) {
 	res = BigInt(0);
-	for (var i=0; i<arr.length; i++) {
+	for (var i = 0; i < arr.length; i++) {
 		res += (BigInt(2) ** BigInt(i)) * BigInt(arr[i]);
 	}
 	return res;
 }
 
 // This function will convert a bigInt into the chucks of Integers
-function chunkBigInt(n, mod=BigInt(2**51)){
+function chunkBigInt(n, mod = BigInt(2 ** 51)) {
 	if (!n) return [0];
 	let arr = [];
 	while (n) {
-		arr.push(BigInt(modulus(n,mod)));
+		arr.push(BigInt(modulus(n, mod)));
 		n /= mod;
 	}
 	return arr;
 }
 
-let p = BigInt(2**255) - BigInt(19);
+let p = BigInt(2 ** 255) - BigInt(19);
 let d = 37095705934669439343138083508754565189542113879843219016388785533085940283555n;
 // This function will perform point addition on elliptic curve 25519 to check point addition circom
-function point_add(P,Q){
-  let A =  modulus((P[1]-P[0])*(Q[1]-Q[0]),p);
-  let B =  modulus((P[1]+P[0])*(Q[1]+Q[0]),p); 
-  let C =  modulus(BigInt(2)*P[3]*Q[3]*d , p);
-  let D =  modulus(BigInt(2)*P[2]*Q[2] ,p);
+function point_add(P, Q) {
+	let A = modulus((P[1] - P[0]) * (Q[1] - Q[0]), p);
+	let B = modulus((P[1] + P[0]) * (Q[1] + Q[0]), p);
+	let C = modulus(BigInt(2) * P[3] * Q[3] * d, p);
+	let D = modulus(BigInt(2) * P[2] * Q[2], p);
 
-  let E = B-A;
-  let F = D-C;
-  let G = D+C;
-  let H = B+A;
-  
-  return [E*F, G*H, F*G, E*H];
+	let E = B - A;
+	let F = D - C;
+	let G = D + C;
+	let H = B + A;
+
+	return [E * F, G * H, F * G, E * H];
 }
 //This funciton will give the point multiplcation on EC 25519
-function point_mul(s,P){
-	let Q = [0n,1n,1n,0n];
-	while (s > 0){
-		if (s & 1n){
-			Q = point_add(Q,P);
+function point_mul(s, P) {
+	let Q = [0n, 1n, 1n, 0n];
+	while (s > 0) {
+		if (s & 1n) {
+			Q = point_add(Q, P);
 		}
-		P = point_add(P,P);
+		P = point_add(P, P);
 		s >>= 1n;
 	}
 	return Q;
 }
-function dechunk(x){
+
+function dechunk(x) {
 	sum = 0n;
-	for (let i=0;i<x.length;i++){
-		const m = BigInt(2**51);
-		sum += (m**BigInt(i))*x[i];
+	for (let i = 0; i < x.length; i++) {
+		const m = BigInt(2 ** 51);
+		sum += (m ** BigInt(i)) * x[i];
 	}
 	return sum;
 }
-module.exports = { buffer2bits, convertToEvenLength, normalize, bigIntToLEBuffer, pad, chunkBigInt, bitsToBigInt, point_add, modulus, point_mul, dechunk};
+module.exports = {
+	buffer2bits,
+	convertToEvenLength,
+	normalize,
+	bigIntToLEBuffer,
+	pad,
+	chunkBigInt,
+	bitsToBigInt,
+	point_add,
+	modulus,
+	point_mul,
+	dechunk
+};
