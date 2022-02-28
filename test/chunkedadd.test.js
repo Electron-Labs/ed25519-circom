75f91a63a6c813a1f703fa86d60b284df06d2c84
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2022-02-28 22:52:03 +0530

    test: add fast-check test chunkedadd against four random number

--- test/chunkedadd.test.js
+++ test/chunkedadd.test.js
@@ -2,6 +2,7 @@ const path = require('path');
 const assert = require('assert');
 const wasmTester = require('circom_tester').wasm;
 const utils = require('./utils');
+const { default: fc } = require('fast-check');
 
 describe('base 51 addition test', () => {
   describe('when performing chuncked addition on three 200 bits numbers', () => {
@@ -17,6 +18,23 @@ describe('base 51 addition test', () => {
       const witness = await (await cir).calculateWitness({ in: [chunk1, chunk2, chunk3] }, true);
       const expected = utils.chunkBigInt(a + b + c);
       assert.ok(witness.slice(1, 5).every((u, i) => u === expected[i]));
+    });
+  });
+  describe('when performing chunked addition on four randomly genrated 200 bits numbers', () => {
+    it('should add them correctly', async () => {
+      const cir = wasmTester(path.join(__dirname, 'circuits', 'chunkedadd1.circom'));
+      await fc.assert(
+        fc.asyncProperty(fc.bigInt(2n, BigInt(2 ** 200) - 19n), fc.bigInt(2n, BigInt(2 ** 200) - 27n), fc.bigInt(2n, BigInt(2 ** 200) - 35n), fc.bigInt(2n, BigInt(2 ** 200) - 45n), async (a, b, c, d) => {
+          const chunk1 = utils.pad(utils.chunkBigInt(a), 4);
+          const chunk2 = utils.pad(utils.chunkBigInt(b), 4);
+          const chunk3 = utils.pad(utils.chunkBigInt(c), 4);
+          const chunk4 = utils.pad(utils.chunkBigInt(d), 4);
+
+          const witness = await (await cir).calculateWitness({ in: [chunk1, chunk2, chunk3, chunk4] }, true);
+          const expected = utils.chunkBigInt(a + b + c + d);
+          witness.slice(1, 9).every((u, i) => u === expected[i]);
+        }),
+      );
     });
   });
 });
