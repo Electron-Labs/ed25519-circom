7c65ef134bd6a7191df6867efd2307cc3be0e9d5
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2022-02-28 23:01:06 +0530

    fix: formatting modinv test

--- test/modinv.test.js
+++ test/modinv.test.js
@@ -6,36 +6,32 @@ const utils = require('./utils');
 const fc = require('fast-check');
 const { expect } = require('chai');
 
-
-describe("Inverse Modulo test for base51", () => {
-    describe("When Performing inverse modulo on a 255 bit number", () => {
-        const p = BigInt(2**255)-BigInt(19);
-        it("Should calculate the inverse correctly", async() => {
-            const cir = await wasmTester(path.join(__dirname,"circuits","modinv.circom"));
-            const   a = BigInt(2**255)-BigInt(20);
-            const chunk = utils.chunkBigInt(a);
-            const witness = await cir.calculateWitness({ in:chunk }, true);
-            const inv = bigintModArith.modInv(a, p);
-            const expected = utils.chunkBigInt(inv);
-            assert.ok(witness.slice(1 ,6).every((u ,i) => {
-                return u === expected[i];
-            }));
-        });
+describe('Inverse Modulo test for base51', () => {
+  describe('When Performing inverse modulo on a 255 bit number', () => {
+    const p = BigInt(2 ** 255) - BigInt(19);
+    it('Should calculate the inverse correctly', async () => {
+      const cir = await wasmTester(path.join(__dirname, 'circuits', 'modinv.circom'));
+      const a = BigInt(2 ** 255) - BigInt(20);
+      const chunk = utils.chunkBigInt(a);
+      const witness = await cir.calculateWitness({ in: chunk }, true);
+      const inv = bigintModArith.modInv(a, p);
+      const expected = utils.chunkBigInt(inv);
+      assert.ok(witness.slice(1, 6).every((u, i) => u === expected[i]));
     });
-    describe("When performing inverse modulo on numbers less then p - 1 ", () => {
-			const p = BigInt(2**255)-BigInt(19);
+  });
+  describe('When performing inverse modulo on numbers less then p - 1 ', () => {
+    const p = BigInt(2 ** 255) - BigInt(19);
 
-			it("Should calculate the inverse correctly", async() => {
-				const cir = await wasmTester(path.join(__dirname,"circuits","modinv.circom"));
-				await fc.assert(
-						fc.asyncProperty(fc.bigInt(1n, p-1n), async (data) => {
-								const witness = await cir.calculateWitness({in:utils.chunkBigInt(data)},true)
-								const expected = utils.chunkBigInt(bigintModArith.modInv(data, p));
-								witness.slice(1,6).every((u, i) => {
-										return u === expected[i];
-								})
-						}) 
-				);
-			});
-		});
+    it('Should calculate the inverse correctly', async () => {
+      const cir = await wasmTester(path.join(__dirname, 'circuits', 'modinv.circom'));
+      await fc.assert(
+        fc.asyncProperty(fc.bigInt(1n, p - 1n), async (data) => {
+          const witness = await cir.calculateWitness({ in: utils.chunkBigInt(data) }, true);
+          const expected = utils.chunkBigInt(bigintModArith.modInv(data, p));
+          witness.slice(1, 6).every((u, i) => u === expected[i]);
+        }),
+      );
+    });
+  });
 });
+
